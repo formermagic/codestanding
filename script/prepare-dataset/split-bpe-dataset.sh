e00cc79279f414714b5926becf100fe9dc92fa45
Eugene Mozharovsky <mozharovsky@live.com>
2019-12-05 23:10:22 +0300

    feat(script): (wip) add a split-dataset phase script

---
+++ script/prepare-dataset/split-bpe-dataset.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# TODO:
+# 1) Create a tmp dir for train/valid/test splits
+# 2) Create train/valid/test splits from full train files
+# 3) Replace full train files with train/valid/test splits
