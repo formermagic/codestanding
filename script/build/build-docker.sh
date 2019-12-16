9e6146e6d25e9b25643375c6d044baf31c7f1eb5
Eugene Mozharovsky <mozharovsky@live.com>
2019-12-16 03:29:55 +0300

    fix(script): add docker-compose install steps

--- script/build/build-docker.sh
+++ script/build/build-docker.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-docker-compose -f ./.devcontainer/docker-compose.yml config --services
+echo "Installing docker-compose..."
+sudo curl -L "https://github.com/docker/compose/releases/download/1.25.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 
+echo "Building..."
+docker-compose -f ./.devcontainer/docker-compose.yml config --services
 docker-compose --project-name codestanding_devcontainer -f ./.devcontainer/docker-compose.yml up -d --build
