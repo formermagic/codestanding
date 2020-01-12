069e1ded5d68890905ff924659defc381f7bcddc
Eugene Mozharovsky <mozharovsky@live.com>
2020-01-13 02:01:48 +0300

    feat(utils): add a method to parse listed arguments

--- src/utils.py
+++ src/utils.py
@@ -10,5 +10,10 @@ def iterate_lines(file_path: str) -> typing.Generator[str, None, None]:
             yield line
 
 
+def parse_listed_arg(arg: str, separator: str = ",") -> typing.List[typing.Any]:
+    tokens = [token.strip() for token in arg.split(separator)]
+    return tokens
+
+
 if __name__ == "__main__":
     pass
