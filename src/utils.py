2110c712d1d192b408a44959991fc6c91275ee78
Eugene Mozharovsky <mozharovsky@live.com>
2020-01-31 02:14:18 +0300

    feat(utils): add a method to count lines in a files

--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,5 @@
+import os
+import subprocess
 import typing
 
 
@@ -13,6 +15,14 @@ def iterate_lines(file_path: str) -> typing.Generator[str, None, None]:
 def parse_listed_arg(arg: str, separator: str = ",") -> typing.List[typing.Any]:
     tokens = [token.strip() for token in arg.split(separator)]
     return tokens
+
+
+def lines_in_file(filepath: str) -> int:
+    if not os.path.exists(filepath):
+        raise FileNotFoundError(filepath)
+    output = subprocess.check_output(f"wc -l < {filepath}", shell=True)
+    n_lines = int(output.strip())
+    return n_lines
 
 
 if __name__ == "__main__":
