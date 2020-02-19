7af0113d7e6299331a5e68468743005a8046af22
Eugene Mozharovsky <mozharovsky@live.com>
2020-02-19 03:02:43 +0300

    feat(mass_unsup): add a masked dictionary class impl

---
+++ src/mass_unsupervised/masked_dictionary.py
@@ -0,0 +1,29 @@
+from fairseq.data import Dictionary
+
+
+class MaskedDictionary(Dictionary):
+    def __init__(
+        self, pad="<pad>", eos="</s>", unk="<unk>", mask="<mask>",
+    ) -> None:
+        super().__init__(pad, eos, unk)
+        self.mask_word = mask
+        self.mask_index = self.add_symbol(mask)
+        self.nspecial = len(self.symbols)
+
+    def mask(self) -> int:
+        return self.mask_index
+
+    @classmethod
+    def load(cls, filepath: str) -> "MaskedDictionary":
+        dictionary = cls()
+
+        with open(
+            filepath, mode="r", encoding="utf-8", errors="ignore"
+        ) as input_file:
+            for line in input_file:
+                key, _ = line.split()
+                dictionary.add_symbol(key)
+
+        dictionary.nspecial = 199
+
+        return dictionary
