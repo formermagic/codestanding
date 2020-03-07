f8c48a5178ebaec6b5fd7a1d962de8f4e35d41b6
Eugene Mozharovsky <mozharovsky@live.com>
2020-03-07 03:00:22 +0300

    feat(mass_task): prepare eval language pair datasets

--- src/mass_unsupervised/unsupervised_mass_task.py
+++ src/mass_unsupervised/unsupervised_mass_task.py
@@ -370,6 +370,35 @@ class UnsupervisedMASSTask(FairseqTask):
                 max_target_positions=self.args.max_target_positions,
             )
 
+        eval_para_dataset = {}
+        if split != "train":
+            for lang_pair in self.args.valid_lang_pairs:
+                src, tgt = lang_pair.split("-")
+                if src == tgt:
+                    src_key = src + "-" + tgt
+                    tgt_key = src + "-" + tgt
+                    src_dataset = src_mono_datasets[src_key]
+                    tgt_dataset = src_mono_datasets[tgt_key]
+                else:
+                    key = "-".join(sorted([src, tgt]))
+                    src_key = key + "." + src
+                    tgt_key = key + "." + tgt
+                    src_dataset = src_para_datasets[src_key]
+                    tgt_dataset = src_para_datasets[tgt_key]
+
+                eval_para_dataset[lang_pair] = LanguagePairDataset(
+                    src_dataset,
+                    src_dataset.sizes,
+                    self.dicts[src],
+                    tgt_dataset,
+                    tgt_dataset.sizes,
+                    self.dicts[tgt],
+                    left_pad_source=self.args.left_pad_source,
+                    left_pad_target=self.args.left_pad_target,
+                    max_source_positions=self.args.max_source_positions,
+                    max_target_positions=self.args.max_target_positions,
+                )
+
         )
 
 
