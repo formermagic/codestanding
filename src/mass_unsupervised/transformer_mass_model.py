126b36d72fae9df3532d7bcba00a082fab445e22
Eugene Mozharovsky <mozharovsky@live.com>
2020-03-30 00:13:27 +0300

    feat(mass_model): add namespace arg for encoder embed dim

--- src/mass_unsupervised/transformer_mass_model.py
+++ src/mass_unsupervised/transformer_mass_model.py
@@ -368,6 +368,7 @@ def base_architecture(args: Namespace) -> None:
     args.activation_fn = getattr(args, "activation_fn", "relu")
 
     args.encoder_layers = getattr(args, "encoder_layers", 6)
+    args.encoder_embed_path = getattr(args, "encoder_embed_path", None)
     args.encoder_embed_dim = getattr(args, "encoder_embed_dim", 512)
     args.encoder_ffn_embed_dim = getattr(args, "encoder_ffn_embed_dim", 2048)
     args.encoder_attention_heads = getattr(args, "encoder_attention_heads", 8)
