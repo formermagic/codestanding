fb220026c707ddc2ebf76b000751bdd18f4ff541
mozharovsky <mozharovsky@live.com>
2020-04-19 04:15:31 +0300

    feat(contrib/data): add module init file

---
+++ fairseq_contrib/data/__init__.py
@@ -0,0 +1,4 @@
+from .encoders import BertWordPieceBPE
+from .masked_dictionary import MaskedDictionary
+from .masked_language_pair_dataset import MaskedLanguagePairDataset
+from .noisy_language_pair_dataset import NoisyLanguagePairDataset
