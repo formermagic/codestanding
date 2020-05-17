b427a2bf4f46f7f3eda0a5a0b295823d0ad008a7
mozharovsky <mozharovsky@live.com>
2020-05-17 05:56:36 +0300

    feat(transformers): add CodeBertDataset class for getting code dataset cached tokens

---
+++ src/transformers/dataset_codebert.py
@@ -0,0 +1,42 @@
+import linecache
+import os
+import subprocess
+from typing import Text
+
+import torch
+from torch.utils.data import Dataset
+
+from .tokenization_codebert import CodeBertTokenizerFast
+
+
+def lines_in_file(filepath: Text) -> int:
+    if not os.path.exists(filepath):
+        raise FileNotFoundError(filepath)
+    output = subprocess.check_output(f"wc -l < {filepath}", shell=True)
+    n_lines = int(output.strip())
+    return n_lines
+
+
+class CodeBertDataset(Dataset):
+    def __init__(
+        self, tokenizer: CodeBertTokenizerFast, data_path: Text, max_length: int
+    ) -> None:
+        tokenizer._tokenizer.enable_truncation(max_length)
+        self.tokenizer = tokenizer
+        self.data_path = data_path
+        self.max_length = max_length
+        self.num_sentences = lines_in_file(data_path)
+
+    def __len__(self) -> int:
+        return self.num_sentences
+
+    def __getitem__(self, index: int) -> torch.Tensor:
+        line = linecache.getline(self.data_path, index).rstrip()
+        batch_encoding = self.tokenizer.encode_plus(
+            line, add_special_tokens=True, max_length=self.max_length
+        )
+
+        # pylint: disable=not-callable
+        input_ids = torch.tensor(batch_encoding["input_ids"], dtype=torch.long)
+
+        return input_ids
