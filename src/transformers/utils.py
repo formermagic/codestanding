03ac8b7ab48b95465df8f4ee235d8f02f0862809
mozharovsky <mozharovsky@live.com>
2020-05-19 00:41:10 +0300

    refactor(transformers): cast to strict types properly

--- src/transformers/utils.py
+++ src/transformers/utils.py
@@ -1,29 +1,34 @@
-from typing import Any, Optional
+from typing import Any, Optional, cast
 
 import numpy as np
 import torch
 
 
 def safe_round(number: Any, ndigits: int) -> float:
+    round_number: float
     if hasattr(number, "__round__"):
-        return round(number, ndigits)
+        round_number = round(number, ndigits)
     elif torch.is_tensor(number) and number.numel() == 1:
-        return safe_round(number.item(), ndigits)
+        round_number = safe_round(number.item(), ndigits)
     elif np.ndim(number) == 0 and hasattr(number, "item"):
-        return safe_round(number.item(), ndigits)
+        round_number = safe_round(number.item(), ndigits)
     else:
-        return number
+        round_number = number
+    return round_number
 
 
+# pylint: disable=not-callable
 def get_perplexity(
     loss: Optional[Any], ndigits: int = 2, base: int = 2
 ) -> torch.FloatTensor:
+    ppl_tensor: torch.Tensor
     if loss is None:
-        # type: ignore
-        return torch.FloatTensor([0.0])
+        ppl_tensor = torch.tensor([0.0])
     try:
-        # type: ignore
-        return torch.FloatTensor([safe_round(base ** loss, ndigits)])
+        ppl_tensor = torch.tensor([safe_round(base ** loss, ndigits)])
     except OverflowError:
-        # type: ignore
-        return torch.FloatTensor([float("inf")])
+        ppl_tensor = torch.tensor([float("inf")])
+    else:
+        ppl_tensor = torch.tensor([float("inf")])
+
+    return cast(torch.FloatTensor, ppl_tensor.float())
