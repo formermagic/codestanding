668ff1c920be26598fb7058733d97c4dc67992a4
mozharovsky <mozharovsky@live.com>
2020-05-24 22:35:09 +0300

    feat(transformers): add a class for preprocessing raw text dataset files

--- src/transformers/indexed_dataset.py
+++ src/transformers/indexed_dataset.py
@@ -101,3 +101,44 @@ class IndexProcessorMixin:
                 result.append(size)
                 seek += (size + 1) * 2
 
+
+class IndexDatasetPreprocessor(IndexProcessorMixin):
+    def __init__(
+        self, filepath: Text, tokenizer: PreTrainedTokenizer, max_length: int
+    ) -> None:
+        super().__init__(tokenizer.vocab_size)
+        self.filepath = filepath
+        self.tokenizer = tokenizer
+        self.max_length = max_length
+
+    def preprocess(self, output_path: Text) -> None:
+        filename = os.path.basename(self.filepath).split(".")[0]
+        output_filepath = os.path.join(output_path, f"{filename}.bin")
+        input_file = open(self.filepath, mode="r", encoding="utf-8")
+        output_file = open(output_filepath, mode="wb")
+
+        def flush(bucket: List[Text], file: IO[AnyStr]) -> None:
+            batch_encoding = self.tokenizer.batch_encode_plus(
+                bucket, add_special_tokens=True, max_length=self.max_length
+            )
+            batch_ids = batch_encoding.data["input_ids"]
+
+            for tokens in batch_ids:
+                _tokens = [len(tokens)] + tokens
+                _bytes = self.pack(_tokens)
+                file.write(_bytes)
+
+        num_lines = lines_in_file(self.filepath)
+        kwargs = {"total": num_lines, "desc": "Dataset Preprocessing"}
+
+        with input_file, output_file:
+            bucket = []
+            for line in tqdm(input_file, **kwargs):
+                line = line.rstrip()
+                bucket.append(line)
+                if len(bucket) == 2 ** 16:
+                    flush(bucket, output_file)
+                    bucket = []
+            if bucket:
+                flush(bucket, output_file)
+
